2b47a9ab4962cc90c48c576a777c4e76f52bb4f7
EduContin <eduardo.contin.04@gmail.com>
2024-06-27 19:08:44 -0300

    Adds `database.js` and `.env`

--- app/api/v1/status/route.tsx
+++ app/api/v1/status/route.tsx
@@ -1,4 +1,9 @@
+import database from "../../../../infra/database";
+
 export async function GET() {
+  const result = await database.query("SELECT 1 + 1 as SUM;");
+  console.log(result.rows);
+
   return Response.json({
     status: "ok",
     version: "1.0.0",
