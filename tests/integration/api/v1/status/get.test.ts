f11ca8439a0a680966a751204f0bdcf833a43187
EduContin <eduardo.contin.04@gmail.com>
2024-06-29 13:37:20 -0300

    Finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.ts
+++ tests/integration/api/v1/status/get.test.ts
@@ -4,4 +4,17 @@ test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  // Check if the response body has the expected properties
+  expect(responseBody.update_at).toBeDefined();
+
+  // Check if the updatedAt property is a valid date
+  const parsedUpdatedAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.3");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
